5eab47873dee57d1166208680de334c08346db26
Jorge Chavez <echavez@nearsoft.com>
2016-04-11 16:09:45 -0600

    Replace the hardcode port

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ io.on('connection', function(socket){
 });
 
 http.listen(port, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:'+port);
 });
 
 // catch 404 and forward to error handler
