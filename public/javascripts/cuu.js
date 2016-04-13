b7c64d1d7c71943121b63e918aaf2ba2090e5814
echavezNS <echavez@nearsoft.com>
2016-04-13 01:14:05 -0600

    Remove STUN server

--- public/javascripts/cuu.js
+++ public/javascripts/cuu.js
@@ -1,71 +1,71 @@
 'use strict';
 $(document).ready(function() {
-  var socket = io();
+    var socket = io();
 
-  var cuuvideo  = document.getElementById('cuuvideo');
-  var hilovideo  = document.getElementById('hilovideo');
+    var cuuvideo  = document.getElementById('cuuvideo');
+    var hilovideo  = document.getElementById('hilovideo');
 
-  var constraints = window.constraints = {
-    video: true,
-    audio: true
-  };
-
-  navigator.getUserMedia(constraints, success, fail);
-
-  function success(stream){
-    var url = window.URL.createObjectURL(stream);
-    hilovideo.src = url;
-    createPeerConnections(stream);
-  }
-
-  function fail(error){
-    console.error("Error: ",error);
-  }
-
-
-  function createPeerConnections(stream){
-
-    var config = {
-      "iceServers": [{"url": "stun:stun.l.google.com:19302"}]
+    var constraints = window.constraints = {
+        video: true,
+        audio: true
     };
-    var pc_config = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 
-    var localPeer = new RTCPeerConnection(config);
+    navigator.getUserMedia(constraints, success, fail);
 
-    localPeer.onicecandidate = function gotMyIceCandidate(evt){
-      if(evt.candidate){
-        socket.emit("ice candidate cuu", evt.candidate);
-      }
+    function success(stream) {
+        var url = window.URL.createObjectURL(stream);
+        hilovideo.src = url;
+        createPeerConnections(stream);
     }
 
-    localPeer.addStream(stream);
-
-    localPeer.createOffer(function gotLocalDescription(desc){
-
-      localPeer.setLocalDescription(desc);
-
-      socket.emit("remote description", desc);
-
-      socket.on("hermosillo answer", function(desc){
-        localPeer.setRemoteDescription(new RTCSessionDescription(desc));
-      });
-
-    },function(error){
-      console.log("Error in offer: ",error);
-    });
-
-    socket.on("ice candidate hilo", function(candidate){
-      var can = new RTCIceCandidate(candidate);
-      localPeer.addIceCandidate(can);
-    });
-
-    localPeer.onaddstream = function(evt){
-      console.log("Recived stream from Hermosillo");
-      console.log(evt);
-      var url = window.URL.createObjectURL(evt.stream);
-      cuuvideo.src = url;
+    function fail(error) {
+        console.error('Error: ', error);
     }
 
-  }
+
+    function createPeerConnections(stream) {
+
+        var config = {
+            'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]
+        };
+        var pc_config = {'iceServers': [{'url': 'stun:stun.l.google.com:19302'}]};
+
+        var localPeer = new RTCPeerConnection(config);
+
+        localPeer.onicecandidate = function gotMyIceCandidate (evt) {
+            if(evt.candidate) {
+                socket.emit('ice candidate cuu', evt.candidate);
+            }
+        }
+
+        localPeer.addStream(stream);
+
+        localPeer.createOffer(function gotLocalDescription (desc) {
+
+            localPeer.setLocalDescription(desc);
+
+            socket.emit('remote description', desc);
+
+            socket.on('hermosillo answer', function(desc) {
+                localPeer.setRemoteDescription(new RTCSessionDescription(desc));
+            });
+
+        }, function(error) {
+            console.log('Error in offer: ', error);
+        });
+
+        socket.on('ice candidate hilo', function(candidate) {
+            var can = new RTCIceCandidate(candidate);
+            localPeer.addIceCandidate(can);
+        });
+
+        localPeer.onaddstream = function(evt) {
+            console.log('Recived stream from Hermosillo');
+            console.log(evt);
+            var url = window.URL.createObjectURL(evt.stream);
+            cuuvideo.src = url;
+        }
+
+    }
 
 });
