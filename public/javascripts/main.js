a5fb484776c827662d52d91c65decff6d4dd0c52
echavezNS <echavez@nearsoft.com>
2016-04-13 01:25:35 -0600

    Hide local preview handler

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,16 +2,16 @@
 
 $(document).ready(function() {
 
-  setLayout();
-  $(document).resize(function() {
     setLayout();
-  });
-  function setLayout(){
-    $(".remote").height($(window).height());
-  }
+    $(document).resize(function() {
+        setLayout();
+    });
+    function setLayout() {
+        $('.remote').height($(window).height());
+    }
 
-  $("#hide-local-preview").click(function(){
-    $(".r-b-corner").toggleClass("hidden");
-  });
+    $('#hide-local-preview').click(function() {
+        $('.r-b-corner').toggleClass('hidden');
+    });
 
 });
