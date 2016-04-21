48710916a8b533f428ffe3a4be207dc5e33ced2a
Jorge Chavez <echavez@nearsoft.com>
2016-04-21 11:53:09 -0600

    Remove https

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var keys = {
   key: fs.readFileSync('ssl/key.pem'),
   cert: fs.readFileSync('ssl/cert.pem')
 };
-var http = require('https').Server(keys, app);;
+var http = require('http').Server(app);;
 var io = require('socket.io')(http);
 
 var current = 0;
