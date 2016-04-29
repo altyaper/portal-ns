869305823b579644adee3c85d67137a70ac19994
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-04-28 18:41:39 -0700

    add soun effect when the portal is turned on or off

--- public/javascripts/portal.js
+++ public/javascripts/portal.js
@@ -12,7 +12,9 @@ var socket = io(),
     audio,
     portalparam = window.location.search.split('=')[1],
     portal,
-    animation;
+    animation,
+    audioOn = document.getElementById('portalOn'),
+    audioOff = document.getElementById('lportalOff');
 
 
 var portales = {
@@ -76,6 +78,7 @@ socket.on('talk', function(evt) {
     // video = tracks[0];
     // audio = tracks[1];
     //
+    portalOn.play();
     video.enabled = true;
     audio.enabled = true;
     
@@ -89,6 +92,7 @@ socket.on('quiet', function(evt) {
     video = tracks[0];
     audio = tracks[1];
 
+    portalOff.play();
     video.enabled = false;
     audio.enabled = false;
 
