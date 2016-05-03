7572dd2e3baee8db727336121eb990d5f330cbff
Jorge Chavez <echavez@nearsoft.com>
2016-05-03 11:35:15 -0600

    Change config logic

--- config/index.js
+++ config/index.js
@@ -2,17 +2,13 @@
 
 var host;
 
-if(process.env.NODE_ENV == "production"){
-
+if(process.env.APP_NAME){
   host = "https://"+process.env.APP_NAME+".herokuapp.com";
-  token = process.env.TOKEN;
-
 }else{
-
-  host = "http://localhost:5000";
-  token = '1B724F94C3EDC1DA6FD7294D1C611';
-
+  host = "https://dev-portal-ns.herokuapp.com";
 }
+
+token = process.env.TOKEN || '1B724F94C3EDC1DA6FD7294D1C611';
 
 var config = {
   host: host,
